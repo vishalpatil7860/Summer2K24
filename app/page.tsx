aa74626aae842a886ae8f3eb1d669670ebb34651
Vishal Patil <vishalpatil7860@gmail.com>
2024-04-30 13:44:38 -0400

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
-import Skills from "@/components/skills";
+// import Skills from "@/components/skills";
 
 export default function Home() {
   return (
